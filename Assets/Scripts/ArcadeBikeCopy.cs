8b8d8d824f876075b707aa4ba02a45e25cc6ce7b
isaachraga <isaachraga@yahoo.com>
2024-09-19 10:23:11 -0400

    sliding added for motorcycle

--- Assets/Scripts/ArcadeBikeCopy.cs
+++ Assets/Scripts/ArcadeBikeCopy.cs
@@ -14,9 +14,10 @@ namespace ArcadeBP2
         public groundCheck GroundCheck;
         public LayerMask drivableSurface;
 
-        public float MaxSpeed, accelaration, brake, turn;
+        public float MaxSpeed, accelaration, brake, turn, driftLimit, slide;
 
-        public bool drifting, driftLeft;
+        public bool drifting, sliding;
+        public Vector3 slidePos, slideRot;
         public Rigidbody rb, bikeBody;
 
 
@@ -93,7 +94,12 @@ namespace ArcadeBP2
            
             bikeVelocity = bikeBody.transform.InverseTransformDirection(bikeBody.velocity);
             
-            
+            if(Input.GetKey(KeyCode.I)){
+                sliding = true;
+            }else{
+                sliding = false;
+            }
+
             DriftCheck();
 
             if (Mathf.Abs(bikeVelocity.x) > 0)
@@ -101,11 +107,11 @@ namespace ArcadeBP2
                 
                 //changes friction according to sideways speed of bike
                 if(drifting){
-                    print("drifting");
+                    //print("drifting");
                     frictionMaterial.dynamicFriction = driftFrictionCurve.Evaluate(Mathf.Abs(bikeVelocity.x / 100));
                 } else{
                     frictionMaterial.dynamicFriction = frictionCurve.Evaluate(Mathf.Abs(bikeVelocity.x / 100));
-                    print("not drifting");
+                    //print("not drifting");
                 }
                 
             }
@@ -116,56 +122,62 @@ namespace ArcadeBP2
                 //turnlogic
                 float sign = Mathf.Sign(bikeVelocity.z);
                 float TurnMultiplyer = turnCurve.Evaluate(bikeVelocity.magnitude / MaxSpeed);
-                if (accelerateInput > 0.1f || bikeVelocity.z > 1)
-                //if ( bikeVelocity.z > 1)
-                {
-                    bikeBody.AddTorque(Vector3.up * horizontalInput * sign * turn * 10 * TurnMultiplyer);
-                }
-                //need to figure out if drifting is mixed with breaking is mixed with reverse
-                //else if (brakeInput > 0.1f || bikeVelocity.z < -1)
-                else if ( bikeVelocity.z < -1)
-                {
-                    bikeBody.AddTorque(Vector3.up * horizontalInput * sign * turn * 10 * TurnMultiplyer);
-                }
+                if(sliding){
+                    print("sliding");
+                    rb.velocity = Vector3.Lerp(rb.velocity, bikeBody.transform.forward * 0, slide / 10 * Time.deltaTime);
+                }else{
+                    if (accelerateInput > 0.1f || bikeVelocity.z > 1)
+                    //if ( bikeVelocity.z > 1)
+                    {
+                        bikeBody.AddTorque(Vector3.up * horizontalInput * sign * turn * 10 * TurnMultiplyer);
+                    }
+                    //need to figure out if drifting is mixed with breaking is mixed with reverse
+                    //else if (brakeInput > 0.1f || bikeVelocity.z < -1)
+                    else if ( bikeVelocity.z < -1)
+                    {
+                        bikeBody.AddTorque(Vector3.up * horizontalInput * sign * turn * 10 * TurnMultiplyer);
+                    }
 
-                //brakelogic
-                //if (Input.GetAxis("Jump") > 0.1f)
-                if (!grounded())
-                {
-                    rb.constraints = RigidbodyConstraints.FreezeRotationX;
-                }
-                else
-                {
-                    rb.constraints = RigidbodyConstraints.None;
-                }
+                    //brakelogic
+                    //if (Input.GetAxis("Jump") > 0.1f)
+                    if (!grounded())
+                    {
+                        rb.constraints = RigidbodyConstraints.FreezeRotationX;
+                    }
+                    else
+                    {
+                        rb.constraints = RigidbodyConstraints.None;
+                    }
 
+                    
+
+                    //accelaration logic
+
+                    if (movementMode == MovementMode.AngularVelocity)
+                    {
+                        if (Mathf.Abs(accelerateInput) > 0.1f)
+                        {
+                            rb.angularVelocity = Vector3.Lerp(rb.angularVelocity, bikeBody.transform.right * accelerateInput * MaxSpeed / radius, accelaration * Time.deltaTime);
+                        } else if(Mathf.Abs(brakeInput) > 0.1f){
+                            rb.angularVelocity = Vector3.Lerp(rb.angularVelocity, bikeBody.transform.right * brakeInput * MaxSpeed / radius, brake * Time.deltaTime);
+                        }
+                    }
+                    else if (movementMode == MovementMode.Velocity)
+                    {
+                        //if (Mathf.Abs(verticalInput) > 0.1f && Input.GetAxis("Jump") < 0.1f)
+                        if (Mathf.Abs(accelerateInput) > 0.1f)
+                        {
+                            rb.velocity = Vector3.Lerp(rb.velocity, bikeBody.transform.forward * accelerateInput * MaxSpeed, accelaration / 10 * Time.deltaTime);
+                        } else if (Mathf.Abs(brakeInput) > 0.1f){
+                            rb.velocity = Vector3.Lerp(rb.velocity, bikeBody.transform.forward * 0, brake / 10 * Time.deltaTime);
+                            //print("braking");
+                        }
+                    }
+
+                    //body tilt
+                    bikeBody.MoveRotation(Quaternion.Slerp(bikeBody.rotation, Quaternion.FromToRotation(bikeBody.transform.up, hit.normal) * bikeBody.transform.rotation, 0.09f));
+                    }
                 
-
-                //accelaration logic
-
-                if (movementMode == MovementMode.AngularVelocity)
-                {
-                    if (Mathf.Abs(accelerateInput) > 0.1f)
-                    {
-                        rb.angularVelocity = Vector3.Lerp(rb.angularVelocity, bikeBody.transform.right * accelerateInput * MaxSpeed / radius, accelaration * Time.deltaTime);
-                    } else if(Mathf.Abs(brakeInput) > 0.1f){
-                        rb.angularVelocity = Vector3.Lerp(rb.angularVelocity, bikeBody.transform.right * brakeInput * MaxSpeed / radius, brake * Time.deltaTime);
-                    }
-                }
-                else if (movementMode == MovementMode.Velocity)
-                {
-                    //if (Mathf.Abs(verticalInput) > 0.1f && Input.GetAxis("Jump") < 0.1f)
-                    if (Mathf.Abs(accelerateInput) > 0.1f)
-                    {
-                        rb.velocity = Vector3.Lerp(rb.velocity, bikeBody.transform.forward * accelerateInput * MaxSpeed, accelaration / 10 * Time.deltaTime);
-                    } else if (Mathf.Abs(brakeInput) > 0.1f){
-                        rb.velocity = Vector3.Lerp(rb.velocity, bikeBody.transform.forward * 0, brake / 10 * Time.deltaTime);
-                        //print("braking");
-                    }
-                }
-
-                //body tilt
-                bikeBody.MoveRotation(Quaternion.Slerp(bikeBody.rotation, Quaternion.FromToRotation(bikeBody.transform.up, hit.normal) * bikeBody.transform.rotation, 0.09f));
             }
             else
             {
@@ -176,11 +188,13 @@ namespace ArcadeBP2
 
         public void DriftCheck(){
             //print("Bike: "+bikeVelocity.x);
+            
             //print("bike v: "+bikeVelocity.x);
             //if velocity is greater than XXX, turning, breaking, not already drifting
             if(bikeVelocity.z > 0.1f && Mathf.Abs(bikeVelocity.x) > 0.1f && brakeInput > 0.1f &&!drifting){
                 drifting = true;
-            } else if(Math.Abs(bikeVelocity.x) <= 0.5f && drifting){
+            } //else if(Math.Abs(bikeVelocity.x) <= 11f && drifting){
+            else if(Math.Abs(bikeVelocity.x) <= driftLimit && drifting){
                 
                 drifting = false;
             }
@@ -194,15 +208,26 @@ namespace ArcadeBP2
             Wheels[1].localRotation = rb.transform.localRotation;
 
             //Body
-            if (bikeVelocity.z > 1)
-            {
-                BodyMesh.localRotation = Quaternion.Slerp(BodyMesh.localRotation, Quaternion.Euler(0,
-                                   BodyMesh.localRotation.eulerAngles.y, BodyTilt * horizontalInput * leanCurve.Evaluate(bikeVelocity.z / MaxSpeed)), 4f * Time.deltaTime);
+            if(sliding){
+                BodyMesh.localRotation = Quaternion.Slerp(BodyMesh.localRotation, Quaternion.Euler(slideRot.x, slideRot.y, slideRot.z), 6f * Time.deltaTime);
+                BodyMesh.localPosition = Vector3.Lerp(BodyMesh.localPosition, new Vector3(slidePos.x, slidePos.y, slidePos.z), 6f * Time.deltaTime);
+           
+            }else{
+                BodyMesh.localPosition = Vector3.Lerp(BodyMesh.localPosition, new Vector3(0, -0.379f, -0.81f), 6f * Time.deltaTime);
+                if(BodyMesh.localRotation.y > 0f){
+                    BodyMesh.localRotation = Quaternion.Slerp(BodyMesh.localRotation, Quaternion.Euler(0, 0, 0), 4f * Time.deltaTime);
+                }
+                if (bikeVelocity.z > 1)
+                {
+                    BodyMesh.localRotation = Quaternion.Slerp(BodyMesh.localRotation, Quaternion.Euler(0,
+                                    BodyMesh.localRotation.eulerAngles.y, BodyTilt * horizontalInput * leanCurve.Evaluate(bikeVelocity.z / MaxSpeed)), 4f * Time.deltaTime);
+                }
+                else
+                {
+                    BodyMesh.localRotation = Quaternion.Slerp(BodyMesh.localRotation, Quaternion.Euler(0, 0, 0), 4f * Time.deltaTime);
+                }
             }
-            else
-            {
-                BodyMesh.localRotation = Quaternion.Slerp(BodyMesh.localRotation, Quaternion.Euler(0, 0, 0), 4f * Time.deltaTime);
-            }
+            
 
 
 
