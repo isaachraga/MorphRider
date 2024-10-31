78323bd37df28543de4de4efcefe7023821d64da
isaachraga <isaachraga@yahoo.com>
2024-10-30 20:18:25 -0400

    traffic lane switching added

---
+++ Assets/Scripts/SplineJunction.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class SplineJunction : MonoBehaviour
+{
+    [SerializeField]
+    SplineContainer Route;
+
+    [SerializeField]
+    List<int> node;
+
+    [SerializeField]
+    int selection;
+
+    void OnTriggerEnter(Collider col){
+        if(col.gameObject.tag == "Traffic"){
+            Debug.Log("Junction Hit");
+            col.gameObject.GetComponent<TrafficVehicleController>().SwitchRoute(Route[node[selection]]);
+            
+        }
+
+    }
+}
---
+++ Assets/Scripts/TrafficVehicleController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Splines;
+using Unity.Mathematics;
+
+public class TrafficVehicleController : MonoBehaviour
+{
+    [SerializeField] private SplineContainer road;
+
+    private Spline currentSpline;
+
+    private Rigidbody rb;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+        currentSpline = road.Splines[0];
+    }
+
+    public void SwitchRoute(Spline spl)
+    {
+        currentSpline = spl;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //the spline were working with
+        var native = new NativeSpline(currentSpline);
+        //returns nearest (point closest to the spline) and t (tangent of the line)
+        float distance = SplineUtility.GetNearestPoint(native, transform.position, out float3 nearest,out float t);
+
+        transform.position = nearest;
+        
+        Vector3 forward = Vector3.Normalize(native.EvaluateTangent(t));
+        Vector3 up = native.EvaluateUpVector(t);
+
+        //transform.position = Vector3.Lerp(transform.position, forward, Time.deltaTime*speed);
+        
+        var remappedForward = new Vector3(0,0,1);
+        var remappedUp = new Vector3(0,1,0);
+        var axisRemapRotation = Quaternion.Inverse(Quaternion.LookRotation(remappedForward, remappedUp));
+        
+        transform.rotation = Quaternion.LookRotation(forward, up) * axisRemapRotation;
+
+        Vector3 engineForward = transform.forward;
+
+        if (Vector3.Dot(rb.velocity,transform.forward) < 0)
+        {
+            //engineForward *= -1;
+        }
+
+        //rb.velocity = rb.velocity.magnitude * engineForward;
+        rb.velocity = 50 * engineForward;
+    }
+}
